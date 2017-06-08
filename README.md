c31abe5fec939b3269ebd10660b7310b3581ee29
Ivan Fretes <vanfretes@gmail.com>
2017-06-08 02:57:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Mini Translator 
+
+It is a simple translator, that do you not complicate.
+
+### How Use 
+
+### Base / Dependency:
+* Yandex API
+* jQuery 
+
---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('Inicializamos la app');
+	chrome.tabs.getSelected(null, function(tab) {
+		
+		
+	});
+}, false);
